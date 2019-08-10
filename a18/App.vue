86ae2c0573f631f15ec11247298e4ffd5cb72f88
Felipe Acelino <felipeacelino@hotmail.com>
2019-08-10 20:54:59 -0300

    Aula 18 - Vue Cli

---
+++ a18/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{mensagem}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mensagem: "Qualquer mensagem!"
+    };
+  }
+};
+</script>
+
+<style>
+p {
+  color: blue;
+}
+</style>
