b100d299c03a10e5140a1332d562a4b4cc133a0b
Felipe Acelino <felipeacelino@hotmail.com>
2019-08-25 01:42:09 -0300

    Projeto 03 - Ranek - Busca

--- p3-ranek/src/components/ProdutosLista.vue
+++ p3-ranek/src/components/ProdutosLista.vue
@@ -11,19 +11,32 @@
 
 <script>
 import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
 
 export default {
   name: "ProdutosLista",
   data() {
     return {
-      produtos: null
+      produtos: null,
+      produtosPorPagina: 9
     };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    }
   },
   methods: {
     getProdutos() {
-      api.get("/produto").then(resp => {
+      api.get(this.url).then(resp => {
         this.produtos = resp.data;
       });
+    }
+  },
+  watch: {
+    url() {
+      this.getProdutos();
     }
   },
   created() {
