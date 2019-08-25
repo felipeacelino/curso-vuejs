ed9e7a3509301a7f471937901f67b63bd1784479
Felipe Acelino <felipeacelino@hotmail.com>
2019-08-25 02:10:26 -0300

    Projeto 03 - Ranek - Estilo busca e inputs

--- p3-ranek/src/components/ProdutosBuscar.vue
+++ p3-ranek/src/components/ProdutosBuscar.vue
@@ -1,6 +1,6 @@
 <template>
   <form action>
-    <input type="search" name="busca" id="busca" v-model="busca" />
+    <input type="search" name="busca" id="busca" v-model="busca" placeholder="Buscar..." />
     <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos" />
   </form>
 </template>
@@ -25,5 +25,33 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+#busca:hover,
+#busca:focus {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+  box-shadow: none;
+}
 </style>
