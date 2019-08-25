975d023724162e533fb3b3881531d25c14b32a79
Felipe Acelino <felipeacelino@hotmail.com>
2019-08-25 00:54:12 -0300

    Projeto 03 - Ranek - Produtos

--- p3-ranek/src/components/ProdutosLista.vue
+++ p3-ranek/src/components/ProdutosLista.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>ProdutosLista</div>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
+      <p>{{produto.preco}}</p>
+      <h2>{{produto.nome}}</h2>
+      <p>{{produto.descricao}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "ProdutosLista"
+  name: "ProdutosLista",
+  data() {
+    return {
+      produtos: null
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+        .then(resp => resp.json())
+        .then(json => {
+          this.produtos = json;
+        });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
 };
 </script>
 
