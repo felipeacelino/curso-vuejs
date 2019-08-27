bea83f5b452e23572c24cb6e3442eabda6012fda
Felipe Acelino <felipeacelino@hotmail.com>
2019-08-27 00:12:29 -0300

    Projeto 03 - Ranek - Detalhe produto

--- p3-ranek/src/main.js
+++ p3-ranek/src/main.js
@@ -8,6 +8,17 @@ Vue.config.productionTip = false;
 
 Vue.component("PaginaCarregando", PaginaCarregando);
 
+Vue.filter("numeroPreco", valor => {
+  valor = Number(valor);
+  if (!isNaN(valor)) {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    });
+  }
+  return "";
+});
+
 new Vue({
   router,
   store,
