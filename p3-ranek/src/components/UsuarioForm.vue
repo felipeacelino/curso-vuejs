159e65858bfb3364f5e97f35e524d4ccd0584e81
Felipe Acelino <felipeacelino@hotmail.com>
2019-08-30 03:48:29 -0300

    Projeto 03 - Ranek - CEP Api

--- p3-ranek/src/components/UsuarioForm.vue
+++ p3-ranek/src/components/UsuarioForm.vue
@@ -7,7 +7,7 @@
     <label for="senha">Senha</label>
     <input type="password" name="senha" id="senha" v-model="senha" />
     <label for="cep">Cep</label>
-    <input type="text" name="cep" id="cep" v-model="cep" />
+    <input type="text" name="cep" id="cep" v-model="cep" @keyup="preencherCep" />
     <label for="rua">Rua</label>
     <input type="text" name="rua" id="rua" v-model="rua" />
     <label for="numero">Número</label>
@@ -26,6 +26,7 @@
 
 <script>
 import { mapFields } from "@/helpers.js";
+import { getCep } from "@/services.js";
 
 export default {
   name: "UsuarioForm",
@@ -45,6 +46,19 @@ export default {
       base: "usuario",
       mutation: "UPDATE_USUARIO"
     })
+  },
+  methods: {
+    preencherCep() {
+      const cep = this.cep.replace(/\D/g, "");
+      if (cep.length === 8) {
+        getCep(cep).then(resp => {
+          this.rua = resp.data.logradouro;
+          this.bairro = resp.data.bairro;
+          this.cidade = resp.data.localidade;
+          this.estado = resp.data.uf;
+        });
+      }
+    }
   }
 };
 </script>
