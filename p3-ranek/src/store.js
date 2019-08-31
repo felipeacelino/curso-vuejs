6170c3b884659cce62cd8ea3e2e53ac576e448e8
Felipe Acelino <felipeacelino@hotmail.com>
2019-08-31 01:32:46 -0300

    Projeto 03 - Ranek - POST Usuário

--- p3-ranek/src/store.js
+++ p3-ranek/src/store.js
@@ -31,10 +31,14 @@ export default new Vuex.Store({
   },
   actions: {
     getUsuario(context, payload) {
-      api.get(`/usuario/${payload}`).then(resp => {
+      return api.get(`/usuario/${payload}`).then(resp => {
         context.commit("UPDATE_USUARIO", resp.data);
         context.commit("UPDATE_LOGIN", true);
       });
+    },
+    criarUsuario(context, payload) {
+      context.commit("UPDATE_USUARIO", { id: payload.email });
+      return api.post("/usuario", payload);
     }
   }
 });
