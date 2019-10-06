d752ffd861a8a69ff7c9a0c9095eeadccc9cd8fc
Felipe Acelino <felipeacelino@hotmail.com>
2019-10-05 23:54:12 -0300

    Projeto 03 - Ranek - Compras e Vendas

--- p3-ranek/src/views/usuario/UsuarioVendas.vue
+++ p3-ranek/src/views/usuario/UsuarioVendas.vue
@@ -1,12 +1,90 @@
 <template>
-  <p>Lista de vendas</p>
+  <section>
+    <div v-if="vendas">
+      <h2>Vendas</h2>
+      <div class="produtos-wrapper" v-for="(venda, index) in vendas" :key="index">
+        <ProdutoItem v-if="venda.produto" :produto="venda.produto">
+          <p class="vendedor">
+            <span>Comprador:</span>
+            {{venda.comprador_id}}
+          </p>
+        </ProdutoItem>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="venda.endereco">
+            <li v-for="(value, key) in venda.endereco" :key="key">{{key}}: {{value}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+import ProdutoItem from "@/components/ProdutoItem.vue";
+
 export default {
-  name: "UsuarioVendas"
+  name: "UsuarioVendas",
+  components: {
+    ProdutoItem
+  },
+  data() {
+    return {
+      vendas: null
+    };
+  },
+  computed: {
+    ...mapState(["usuario", "login"])
+  },
+  methods: {
+    getVendas() {
+      api
+        .get(`/transacao?vendedor_id=${this.usuario.id}`)
+        .then(resp => {
+          this.vendas = resp.data;
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+    }
+  },
+  watch: {
+    login() {
+      this.getVendas();
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getVendas();
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom: 60px;
+}
+
+h3 {
+  margin: 0;
+  justify-self: end;
+}
 </style>
